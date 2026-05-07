67ea6d6ff6032dc01a14456058dc158e11fed5c5
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-05-07 23:48:06 +0530

    new

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <header>
+        {/* logo */}
+        <div></div>
+        {/* Branding D */}
+        <div></div>
+      </header>
+    </div>
+  )
+}
+
+export default Navbar
